7e5f6eaad1c115d2622d901b3738c09ad0a19dfc
PNNutkung <pipatpol.t@ku.th>
2016-06-09 17:18:04 +0700

    Change text and background of each fragment

--- app/src/main/java/com/example/nut/sampleviewpager/MainActivity.java
+++ app/src/main/java/com/example/nut/sampleviewpager/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.nut.sampleviewpager;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.main_view_pager);
     }
 }
