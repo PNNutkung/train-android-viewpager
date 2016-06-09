9a2af4e6187ea84184cb307ea7dcaafbb5b22ea2
PNNutkung <pipatpol.t@ku.th>
2016-06-09 18:31:48 +0700

    Bind /w button

--- app/src/main/java/com/example/nut/sampleviewpager/MainActivity.java
+++ app/src/main/java/com/example/nut/sampleviewpager/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.nut.sampleviewpager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
+    private Button btn_prev;
+    private Button btn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,22 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(myPagerAdapter);
 
         viewPager.setCurrentItem(2);
+
+        btn_prev = (Button) findViewById(R.id.btn_prev);
+        btn_prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View v) {
+                    viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+                }
+            }
+
+        );
+        btn_next = (Button) findViewById(R.id.btn_next);
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+            }
+        });
     }
 }
